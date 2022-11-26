62ffc7a7d11ca68c56a243462b469867e8a43ac6
SAFAKARSLI <76523617+SAFAKARSLI@users.noreply.github.com>
2022-11-26 12:18:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ var button = document.getElementById('get-note');
 var boxes = document.getElementsByClassName('note');
 
 
-const notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+const notes = ["Hackerrank (DSA Problem Sets)", 
+               "Data Structures / Algorithms study on Geeksforgeeks", 
+               "Reading Quran", 
+               "Reading Risale", 
+               "Listen Preachings of H.efendi or Nouman Ali Khan", 
+               "Learning Arabic", 
+               "Siyer / life of the sahabe", 
+               "Reading / Watching", 
+               "Practice Violin", 
+               "Learn Music Theory / Turkish Makams"];
 
 console.log(boxes)
 button.addEventListener('click', () => {
   
-    for(box of boxes) {
-        box.innerText = getRandomNote(notes) 
-    }
+  var topic = boxes[0].innerText = getRandomNote(notes)
+  var hour = boxes[1].innerText = Math.floor(Math.random()*2) + 1
+
+//    for(box of boxes) {
+//        box.innerText = getRandomNote(notes) 
+//    }
 })
 
 
