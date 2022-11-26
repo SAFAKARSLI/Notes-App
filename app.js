d9c4abdc820abf9e904c201ad47b5688e95e2ec6
SAFAKARSLI <76523617+SAFAKARSLI@users.noreply.github.com>
2022-11-26 12:37:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ var button = document.getElementById('get-note');
 var boxes = document.getElementsByClassName('note');
 
 
-const notes = ["Hackerrank (DSA Problem Sets)", 
-               "Data Structures / Algorithms study on Geeksforgeeks", 
-               "Reading Quran", 
-               "Reading Risale", 
-               "Listen Preachings of H.efendi or Nouman Ali Khan", 
-               "Learning Arabic", 
-               "Siyer / life of the sahabe", 
-               "Reading / Watching", 
-               "Practice Violin", 
-               "Learn Music Theory / Turkish Makams"];
+const notes = ["CARDINAL", 
+               "DSA", 
+               "MENTAL RENEWAL", 
+               "LANGUAGE", 
+               "MUSIC", 
+               "ASTROPHYSICS"];
 
 console.log(boxes)
 button.addEventListener('click', () => {
